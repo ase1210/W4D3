1797c1a00f6467fcffc6cef279b41f5908b177fc
DrewE <andrew.engelstein@gmail.com>
2019-01-30 14:53:23 -0800

    Add #new to UsersController, Add #new, #create to SessionsController

---
+++ 99 Cats Deuce/app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+  def new
+    render :new      #Need to create view
+  end
+
+  def create
+    user = User.find_by_credentials(params[:user][:username],params[:user][:password])
+    if user
+      user.reset_session_token!
+      login!   #To do
+      redirect_to user_url(user)
+    else 
+      flash.now[:error] = ["Invalid Login Credentials"]
+      render :new 
+    end
+  end
+
+  def destroy
+  end
+
+end
---
+++ 99 Cats Deuce/app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+
+  def new
+    render :new   #Still need to creat a view
+  end
+
+  def create
+  end
+
+
+
+end
