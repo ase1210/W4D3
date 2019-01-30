ea7a66fc21440700906502d765a66ccbe4d64013
DrewE <andrew.engelstein@gmail.com>
2019-01-30 14:29:03 -0800

    Add User model with validations, #password=, #password, #is_password?(pw), #reset_session_token!, ::find_by_credentials(un, pw)

--- 99 Cats Deuce/app/models/user.rb
+++ 99 Cats Deuce/app/models/user.rb
@@ -1,2 +1,45 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ApplicationRecord
+  validates :username, :password_digest, :session_token, presence: true
+  validates :username, :session_token, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  def self.find_by_credentials(un, pw)
+    user = User.find_by(username: un)
+    if user && user.is_password?(pw)
+      user
+    else
+      nil
+    end
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  def password=(pw)
+    @password = pw
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def password
+    @password
+  end
+
+  def is_password?(pw)
+    BCrypt::Password.new(self.password_digest).is_password?(pw)
+  end
 end
